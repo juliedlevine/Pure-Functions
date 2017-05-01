667061f2f8a87e1bd082abfafa2ab07bec340113
juliemdyer <juliemdyer@gmail.com>
2017-05-01 14:01:17 -0400

    Removed extraneous code

--- reverse-polish.js
+++ reverse-polish.js
@@ -12,39 +12,33 @@ function reducer(state, input) {
     } else if (input === '+') {
         let arr = state.stack;
         let addLastTwo = [arr[arr.length - 1] + arr[arr.length - 2]];
-        let sliceArray1 = arr.slice(0, arr.length - 1);
-        let sliceArray2 = arr.slice(0, arr.length - 2);
-        let newArray = sliceArray2.concat(addLastTwo);
+        let keep = arr.slice(0, arr.length - 2);
+        let newArray = keep.concat(addLastTwo);
         let newObj = Object.assign({}, state, {stack: newArray});
         return newObj;
     } else if (input === '-') {
         let arr = state.stack;
         let subtractLastTwo = [arr[arr.length - 1] - arr[arr.length - 2]];
-        let sliceArray1 = arr.slice(0, arr.length - 1);
-        let sliceArray2 = arr.slice(0, arr.length - 2);
-        let newArray = sliceArray2.concat(subtractLastTwo);
+        let keep = arr.slice(0, arr.length - 2);
+        let newArray = keep.concat(subtractLastTwo);
         let newObj = Object.assign({}, state, {stack: newArray});
         return newObj;
     } else if (input === '*') {
         let arr = state.stack;
         let multiplyLastTwo = [arr[arr.length - 1] * arr[arr.length - 2]];
-        let sliceArray1 = arr.slice(0, arr.length - 1);
-        let sliceArray2 = arr.slice(0, arr.length - 2);
-        let newArray = sliceArray2.concat(multiplyLastTwo);
+        let keep = arr.slice(0, arr.length - 2);
+        let newArray = keep.concat(multiplyLastTwo);
         let newObj = Object.assign({}, state, {stack: newArray});
         return newObj;
     } else if (input === '/') {
         let arr = state.stack;
         let divideLastTwo = [arr[arr.length - 1] / arr[arr.length - 2]];
-        let sliceArray1 = arr.slice(0, arr.length - 1);
-        let sliceArray2 = arr.slice(0, arr.length - 2);
-        let newArray = sliceArray2.concat(divideLastTwo);
+        let keep = arr.slice(0, arr.length - 2);
+        let newArray = keep.concat(divideLastTwo);
         let newObj = Object.assign({}, state, {stack: newArray});
         return newObj;
     } else if (input === 'q') {
-        let newObj = Object.assign({}, state, {
-            action: 'end'
-        });
+        let newObj = Object.assign({}, state, {action: 'end'});
         return newObj;
     }
 }
